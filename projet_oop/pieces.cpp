30e200c81f25c7bd1f2199680fa03deaaaad274a
JQZ1111 <jiaqi.zhao@polymtl.ca>
2025-04-06 10:53:07 -0400

    Add project files.

---
+++ projet_oop/pieces.cpp
@@ -0,0 +1,37 @@
+
+static bool roiOuNon = false;
+
+class Piece {
+public:
+
+private:
+
+};
+
+class Roi :public Piece {
+public:
+
+private:
+
+};
+
+class Tour:public Piece {
+public:
+
+private:
+
+};
+
+class Cavalier:public Piece {
+public:
+
+private:
+
+};
+
+class RessourceManager {
+public:
+
+private:
+
+};
